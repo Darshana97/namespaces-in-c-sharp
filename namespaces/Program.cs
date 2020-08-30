b05ab66aaac1af766af03036a2abc983eb6f3379
Darshana Senevirathna <39029875+Darshana97@users.noreply.github.com>
2020-08-30 15:13:22 +0530

    Add project files.

---
+++ namespaces/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace namespaces
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ProjectA.TeamA.ClassA.Print();
+            ProjectA.TeamB.ClassA.Print();
+            Console.ReadLine();
+
+
+        }
+    }
+}
+
+namespace ProjectA
+{
+    namespace TeamA
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Hello from class A");
+            }
+        }
+    }
+}
+
+namespace ProjectA
+{
+    namespace TeamB
+    {
+        class ClassA
+        {
+            public static void Print()
+            {
+                Console.WriteLine("Hello from class B");
+            }
+        }
+    }
+}
